20f13f9cea0d0f019d81ff8d835dcb9ed9cd1ffd
raziehl <matei.petroschi@gmail.com>
2023-02-14 18:00:13 +0200

    Added @types/carbon__icons-react

--- src/about/about.tsx
+++ src/about/about.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { CharacterDecimal } from '@carbon/icons-react';
 
 function About() {
   return <div>
     <h2>About Page Test</h2>
+    <CharacterDecimal />
   </div>
 }
 export default About;
