06701aec226815d3d4a27584281b83508fe6a8cc
raziehl <matei.petroschi@gmail.com>
2023-02-14 17:50:54 +0200

    First testing of react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import About from './about/about';
 import './App.css';
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  RouteObject
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload3.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <Routes>
+        {/* <Route path='/' element={< Home />}></Route> */}
+        <Route path='/about' element={< About />}></Route>
+        {/* <Route path='/contact' element={< Contact />}></Route> */}
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/about/about.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function About() {
+  return <div>
+    <h2>About Page Test</h2>
+  </div>
+}
+export default About;
