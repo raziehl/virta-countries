296683e3bc1e6230e1326dfbc02343040efe9f83
raziehl <matei.petroschi@gmail.com>
2023-02-17 15:39:27 +0200

    Added proper flags to countries

--- src/countries/countries.component.tsx
+++ src/countries/countries.component.tsx
@@ -1,12 +1,12 @@
 import { Component } from 'react';
 import { Misuse } from '@carbon/icons-react'
-import getUnicodeFlagIcon from 'country-flag-icons/unicode';
-import { US } from 'country-flag-icons/react/3x2';
+import Flags from 'country-flag-icons/react/3x2';
 
 import { http } from '../core';
 import { CountryDto } from '../core/dtos';
 
 import './countries.component.css';
+
 
 class CountriesComponent extends Component<any, { countries: CountryDto[] }> {
 
@@ -27,18 +27,20 @@ class CountriesComponent extends Component<any, { countries: CountryDto[] }> {
       </div> */}
       <div>
         {
-          this.state?.countries.map((e, i) => 
-            <div className="country" key={e.code}>
+          this.state?.countries.map((e, i) => {
+            const Flag = Flags[e.code as keyof typeof Flags];
+
+            return <div className="country" key={e.code}>
               
               <div className='status-svg'>
-                <svg width="4" height="48" viewBox="0 0 4 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="4" height="48" viewBox="0 0 4 48" fillRule='evenodd' xmlns="http://www.w3.org/2000/svg">
                   <path d="M0 0C2.20914 0 4 1.79086 4 4V44C4 46.2091 2.20914 48 0 48V0Z" fill={ e.otpInAppEnabled ? '#00AD09' : '#FF0000' }/>
                 </svg>
               </div>
 
               <div className='left-content'>
                 <div className='country-flag'>
-                  { getUnicodeFlagIcon(e.code) }
+                  <Flag />
                 </div>
                 <div className='country-name'>
                   <div>{ e.name }</div>
@@ -47,14 +49,18 @@ class CountriesComponent extends Component<any, { countries: CountryDto[] }> {
               </div>
 
               <div className='right-content'>
-                Country Code: { e.code }
+                <div className='country-code'>
+                  Country Code: { e.code }
+                </div>
               </div>
 
               <div className='status'>
-                <Misuse />
+                <div className='country-status'>
+                  <Misuse />
+                </div>
               </div>
             </div>
-          )
+          })
         }
       </div>
     </div>
